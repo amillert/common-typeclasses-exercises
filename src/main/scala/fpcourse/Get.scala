e7d74a4e9ef4a5557b159b9ea6d5e409b3c5f7ca
amillert <devmood.business@gmail.com>
2021-10-10 22:13:01 +0200

    Implemented Get monad

--- src/main/scala/fpcourse/Get.scala
+++ src/main/scala/fpcourse/Get.scala
@@ -19,36 +19,61 @@ import java.nio.{ ByteBuffer, ByteOrder }
 case class Get[A](run: List[Byte] => Either[String, (List[Byte], A)])
 
 object Get {
+  private val defaultErrorMsg: String                      = "Insufficient input"
+  private val defaultLeftErrorMsg: Either[String, Nothing] = defaultErrorMsg.asLeft
+  private lazy val byteGenerator: Gen[List[Byte]]          = Gen.listOf(Gen.oneOf(0, 255).map(_.toByte))
 
   /** TODO 1
     * Consumes n bytes of input parsing no value.
     */
-  def skip(n: Int): Get[Unit] = ???
+  def skip(n: Int): Get[Unit] = Get(_.splitAt(n) match {
+    case (consumed, rest) if consumed.length == n => (rest, ()).asRight
+    case _                                        => defaultLeftErrorMsg
+  })
 
   /** TODO 2
     * True if the input is fully consumed
     */
-  def isEmpty: Get[Boolean] = ???
+  def isEmpty: Get[Boolean] =
+    Get(bytes => (bytes, bytes.isEmpty).asRight)
 
   /** TODO 3
     * Reads one byte from input
     */
-  def getByte: Get[Byte] = ???
+  def getByte: Get[Byte] =
+    // Get(bytes => (bytes, bytes.head).asRight)
+    Get(bytes => bytes.headOption.toRight(defaultErrorMsg).map(bytes.tail -> _))
+  // Get(bytes =>
+  //   (bytes, bytes.headOption) match {
+  //     case (bytes, None) => "no head found".asLeft
+  //     case (bytes, some) => (bytes, some.get).asRight[String]
+  //   }
+  // )
 
   /** TODO 4
     * Reads an Int from input using Big Endian order.
     */
-  def getIntBE: Get[Int] = ???
+  def getIntBE: Get[Int] = getIntInOrder(ByteOrder.BIG_ENDIAN)
 
   /** TODO 5
     * Reads an Int from input using Little Endian order.
     */
-  def getIntLE: Get[Int] = ???
+  def getIntLE: Get[Int] = getIntInOrder(ByteOrder.LITTLE_ENDIAN)
+
+  def getIntInOrder(order: ByteOrder): Get[Int] = Get { bytes =>
+    val (int, remaining) = bytes.splitAt(4)
+    if (int.length == 4) (remaining, bytesToIntUnsafe(int.toArray, order)).asRight
+    else defaultLeftErrorMsg
+  }
 
   /** TODO 6
     * Reads a String of n characters from input.
     */
-  def getString(n: Int): Get[String] = ???
+  def getString(n: Int): Get[String] = Get { bytes =>
+    val (l, r) = bytes.splitAt(n)
+    if (l.length == n) (r, new String(l.toArray)).asRight
+    else defaultLeftErrorMsg
+  }
 
   /** Helper function that turns four bytes into an Int. It doesn't check the
     * length of the array, so please make sure to provide 4 bytes.
@@ -64,9 +89,17 @@ object Get {
     * Instance of monad error for Get.
     */
   implicit val monadGet: MonadError[Get, String] = new MonadError[Get, String] {
-    override def flatMap[A, B](fa: Get[A])(f: A => Get[B]): Get[B] = ???
+    override def flatMap[A, B](fa: Get[A])(f: A => Get[B]): Get[B] =
+      Get(bytes => fa.run(bytes).fold(_.asLeft, tup => f(tup._2).run(tup._1)))
+    // Get { bytes =>
+    //   fa.run(bytes).fmap { case remaining -> res => f(res).run(remaining).toOption.get }
+    //   fa.run(bytes) match {
+    //     case Left(l)                 => l.asLeft
+    //     case Right(remaining -> res) => f(res).run(remaining)
+    //   }
+    // }
 
-    override def pure[A](x: A): Get[A] = ???
+    override def pure[A](x: A): Get[A] = Get(bytes => (bytes, x).asRight)
 
     override def tailRecM[A, B](a: A)(f: A => Get[Either[A, B]]): Get[B] =
       Get { bytes =>
@@ -82,9 +115,10 @@ object Get {
         }
       }
 
-    override def raiseError[A](e: String): Get[A] = ???
+    override def raiseError[A](e: String): Get[A] = Get(_ => e.asLeft)
 
-    override def handleErrorWith[A](fa: Get[A])(f: String => Get[A]): Get[A] = ???
+    override def handleErrorWith[A](fa: Get[A])(f: String => Get[A]): Get[A] =
+      Get(bytes => fa.run(bytes).fold(f(_).run(bytes), _.asRight))
   }
 
   /** TODO 8
@@ -96,10 +130,23 @@ object Get {
     * a generator of List[Byte], then sample it several times (e.g. 32)
     * and check that running both Gets yields the same result every time.
     */
-  implicit def eqGet[A: Eq]: Eq[Get[A]] = ???
+  implicit def eqGet[A: Eq]: Eq[Get[A]] = {
+    case ga1 -> ga2 =>
+      (1 to 32).forall { _ =>
+        val generated = byteGenerator.sample.get
+        ga1.run(generated) === ga2.run(generated)
+      }
+    case _ => false
+  }
 
   /** TODO 9
     * Monoid instance for Get.
     */
-  implicit def monoid[A: Monoid]: Monoid[Get[A]] = ???
+  implicit def monoid[A: Monoid]: Monoid[Get[A]] = new Monoid[Get[A]] {
+
+    override def combine(x: Get[A], y: Get[A]): Get[A] = for { xi <- x; yi <- y } yield xi |+| yi
+
+    override def empty: Get[A] = Monad[Get].pure(Monoid[A].empty)
+
+  }
 }
